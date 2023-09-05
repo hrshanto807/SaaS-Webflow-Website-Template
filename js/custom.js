08a1b96ec8a84d68c59d9ba2a4e3dec32967cc1a
hrsanto <shantosss4907@gmail.com>
2023-09-05 23:08:32 +0600

    home page done

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+(function ($) {
+    $('.sas-testimonial-slider-area').owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true
+    });
+})(jQuery);
